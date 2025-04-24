b753611568e6bfae1e676e4e23702463d7678ad2
Elias Eskelinen <git@eliaseskelinen.fi>
2025-04-24 20:56:17 +0300

    init

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Supernote Inkflow for Wayland via libinput quirks and hwdb";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs =
+    {
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (system: {
+      nixosModules.supernoteWaylandFix = {
+        config = {
+          environment.etc."libinput/local-overrides.quirks".text = ''
+            [Supernote Supernote Nomad]
+            MatchVendor=0x2207
+            MatchProduct=0x07
+            AttrEventCode=-BTN_STYLUS
+          '';
+
+          services.udev.extraHwdb = ''
+            evdev:input:b0003v2207p0007*
+             EVDEV_ABS_00=::20
+             EVDEV_ABS_01=::20
+          '';
+        };
+      };
+    });
+}
