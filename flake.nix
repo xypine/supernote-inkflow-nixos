1d97234d07ef32ebf9c299fbcd53e654b834fe81
Elias Eskelinen <git@eliaseskelinen.fi>
2025-06-24 12:10:53 +0300

    fix: remove unnecessary and broken boilerplate

--- flake.nix
+++ flake.nix
@@ -1,14 +1,9 @@
 {
   description = "Supernote Inkflow for Wayland via libinput quirks and hwdb";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-
   outputs =
+    { self }:
     {
-      self,
-      flake-utils,
-    }:
-    flake-utils.lib.eachDefaultSystem (system: {
       nixosModules.supernoteWaylandFix = {
         config = {
           environment.etc."libinput/local-overrides.quirks".text = ''
@@ -18,7 +13,6 @@
             AttrEventCode=+BTN_STYLUS
             AttrPressureRange=197:194
           '';
-
           services.udev.extraHwdb = ''
             evdev:input:b0003v2207p0007*
              EVDEV_ABS_00=::20
@@ -26,5 +20,5 @@
           '';
         };
       };
-    });
+    };
 }
